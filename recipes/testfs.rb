3b2909bb5d72e7a34494af2bea06971044ae84f9
Phil Viana <phillip.l.viana@gmail.com>
2016-10-26 13:36:03 +0000

    Adding LVM management

--- recipes/testfs.rb
+++ recipes/testfs.rb
@@ -12,3 +12,28 @@ template '/var/lib/sirf/conf.json' do
   source 'multitest.conf.json.erb'
 end
 
+bash 'create_lvm_partition' do
+  user 'root'
+  code <<-EOH
+    echo "n
+p
+1
+
+
+t
+8e
+w
+"|sudo fdisk /dev/sdb;
+    EOH
+end
+
+bash 'create_volumes' do
+  user 'root'
+  code <<-EOH
+    pvcreate /dev/sdb1
+    vgcreate sirfvg /dev/sdb1
+    lvcreate -n lv1 -L 500m sirfvg
+    lvcreate -n lv2 -L 500m sirfvg
+   EOH
+end
+
