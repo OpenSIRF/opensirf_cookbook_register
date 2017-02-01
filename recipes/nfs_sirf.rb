5a64b816931b9776d2288711a2c5522e8bf90e70
Phillip Viana <phillip.l.viana@gmail.com>
2017-02-01 23:34:20 +0000

    Changing permissions and Tomcat port.

--- recipes/nfs_sirf.rb
+++ recipes/nfs_sirf.rb
@@ -3,6 +3,8 @@ include_recipe 'nfs::client4'
 %w(lv1 lv2).each do |lv|
   directory "/var/lib/sirf/storage/nfs/devsirffs/#{lv}" do
     action :create
+    owner 'tomcat_8'
+    group 'tomcat_8'
     recursive true
   end
 
--- recipes/server.rb
+++ recipes/server.rb
@@ -4,6 +4,8 @@
 
 directory '/var/lib/sirf' do
   action :create
+  owner 'tomcat_8'
+  group 'tomcat_8'
 end
 
 yum_package 'java-1.8.0-openjdk-devel.x86_64'
@@ -14,20 +16,14 @@ remote_file '/opt/tomcat_8/webapps/ROOT.war' do
   source node['server_war_url']
 end
 
+cookbook_file '/opt/tomcat_8/conf/server.xml' do
+  source 'server_8088.xml'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
 tomcat_service '8' do
   action :start
 end
 
-#include_recipe 'nfs::client4'
-#
-#%w(lv1 lv2).each do |lv|
-#  directory "/var/lib/sirf/storage/nfs/devsirffs/#{lv}" do
-#    action :create
-#    recursive true
-#  end
-#
-#  mount "#{lv}" do
-#    device "devsirffs:/var/lib/sirf/storage/#{lv}"
-#    mount_point "/var/lib/sirf/storage/nfs/devsirffs/#{lv}"
-#  end
-#end
