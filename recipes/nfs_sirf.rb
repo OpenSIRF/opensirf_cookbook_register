3a9e22935f53a94f95e84f9bedae30037fe03b73
Phillip Viana <phillip.l.viana@gmail.com>
2017-02-05 12:14:34 -0600

    Changing permissions at the end of the configuration.

--- recipes/nfs_sirf.rb
+++ recipes/nfs_sirf.rb
@@ -3,8 +3,8 @@ include_recipe 'nfs::client4'
 %w(lv1 lv2).each do |lv|
   directory "/var/lib/sirf/storage/nfs/devsirffs/#{lv}" do
     action :create
-    owner 'tomcat_opensirf'
-    group 'tomcat_opensirf'
+    owner 'tomcat_8'
+    group 'tomcat_8'
     recursive true
   end
 
@@ -12,5 +12,10 @@ include_recipe 'nfs::client4'
     device "devsirffs:/var/lib/sirf/storage/#{lv}"
     mount_point "/var/lib/sirf/storage/nfs/devsirffs/#{lv}"
   end
+end
+
+execute "chown-data-sirf" do
+  command "chown -R tomcat_8:tomcat_8 /var/lib/sirf"
+  user "root"
 end
 
